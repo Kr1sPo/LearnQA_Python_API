e3372f4a212795bc70251aea150e819d8052f6ce
Kr1sPo <potapovakristin@gmail.com>
2022-10-04 15:23:12 +0300

    Ex17: Негативные тесты на PUT

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,22 +1,75 @@
 import json.decoder
-
+from datetime import datetime
 from requests import Response
 
+
 class BaseCase:
-    def get_cookie(self,response:Response, cookie_name):
+    def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
-    def get_header(self,response:Response, headers_name):
+    def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers, f"Cannot find header with name {headers_name} in the last response"
         return response.headers[headers_name]
 
-    def get_json_value(self,response:Response, name):
+    def get_json_value(self, response: Response, name):
         try:
-            response_as_dict=response.json()
+            response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn't have key {name}"
 
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None, condition=None, firstName="learnqa"):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+
+        if condition is not None:
+            if condition == "no_password":
+                return {
+                    'username': 'learnqa',
+                    'firstName': firstName,
+                    'lastName': 'learnqa',
+                    'email': email
+                }
+            elif condition == "no_username":
+                return {
+                    'password': '1234',
+                    'firstName': firstName,
+                    'lastName': 'learnqa',
+                    'email': email
+                }
+            elif condition == "no_firstName":
+                return {
+                    'password': '1234',
+                    'username': 'learnqa',
+                    'lastName': 'learnqa',
+                    'email': email
+                }
+            elif condition == "no_lastName":
+                return {
+                    'password': '1234',
+                    'username': 'learnqa',
+                    'firstName': firstName,
+                    'email': email
+                }
+            else:
+                return {
+                    'password': '1234',
+                    'username': 'learnqa',
+                    'firstName': firstName,
+                    'lastName': 'learnqa'
+                }
+
+        return {
+            'password': '1234',
+            'username': 'learnqa',
+            'firstName': firstName,
+            'lastName': 'learnqa',
+            'email': email
+        }
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -17,22 +17,8 @@ class TestUserRegister(BaseCase):
         ("no_email")
     ]
 
-    def setup(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
-        self.incorrect_email = f"{base_part}{random_part}{domain}"
-
-
     def test_create_user_successfully(self):
-        data = {
-            'password': '1234',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email,
-        }
+        data = self.prepare_registration_data()
 
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
@@ -41,13 +27,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '1234',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email,
-        }
+        data = self.prepare_registration_data(email)
 
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
@@ -56,13 +36,8 @@ class TestUserRegister(BaseCase):
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
 
     def test_create_user_with_incorrect_email(self):
-        data = {
-            'password': '1234',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.incorrect_email,
-        }
+        incorrect_email = "learnqaexample.com"
+        data = self.prepare_registration_data(incorrect_email)
 
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
@@ -72,17 +47,8 @@ class TestUserRegister(BaseCase):
 
     @pytest.mark.parametrize('condition', exclude_fields)
     def test_create_user_without_any_field(self, condition):
-
-        if condition == "no_password":
-            response = requests.post('https://playground.learnqa.ru/api/user/', data={'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': self.email})
-        elif condition == "no_username":
-            response = requests.post('https://playground.learnqa.ru/api/user/', data={'password': '1234', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': self.email})
-        elif condition == "no_firstName":
-            response = requests.post('https://playground.learnqa.ru/api/user/', data={'password': '1234', 'username': 'learnqa', 'lastName': 'learnqa', 'email': self.email})
-        elif condition == "no_lastName":
-            response = requests.post('https://playground.learnqa.ru/api/user/', data={'password': '1234', 'username': 'learnqa', 'firstName': 'learnqa', 'email': self.email})
-        else:
-            response = requests.post('https://playground.learnqa.ru/api/user/', data={'password': '1234', 'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa'})
+        data = self.prepare_registration_data(None, condition)
+        response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
         field_name = condition.split('_')
 
@@ -93,13 +59,7 @@ class TestUserRegister(BaseCase):
     def test_create_user_with_long_name(self):
         length = random.randint(251, 1000)
         firstName = ''.join(random.choices(string.ascii_letters, k=length))
-        data = {
-            'password': '1234',
-            'username': 'learnqa',
-            'firstName': firstName,
-            'lastName': 'learnqa',
-            'email': self.email,
-        }
+        data = self.prepare_registration_data(None, None, firstName)
 
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
@@ -109,13 +69,8 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_short_name(self):
         firstName = ''.join(random.choices(string.ascii_letters, k=1))
-        data = {
-                'password': '1234',
-                'username': 'learnqa',
-                'firstName': firstName,
-                'lastName': 'learnqa',
-                'email': self.email,
-        }
+        data = self.prepare_registration_data(None, None, firstName)
+
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
         Assertions.assert_code_status(response, 400)
